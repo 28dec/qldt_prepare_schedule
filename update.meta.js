0114172bd163e4ef9ca527f75cee45a556f1477e
Administrator <bachvkhoa@ubuntu.ubuntu-domain>
2019-01-13 21:08:10 +0700

    no change

--- update.meta.js
+++
@@ -1,6 +0,0 @@
-// ==UserScript==
-// @name        Update and Download URL Metadata
-// @version     1.9.1.13e
-// @updateURL   https://raw.githubusercontent.com/28dec/qldt_prepare_schedule/master/update.meta.js
-// @downloadURL https://github.com/28dec/qldt_prepare_schedule/raw/master/tampermonkey.user.js
-// ==/UserScript==
