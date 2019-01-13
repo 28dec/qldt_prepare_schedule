3f0078ef8cdc99d316165cc4956d84573b66cf44
28dec <dcmtaonanlamroi@gmail.com>
2019-01-13 20:38:32 +0700

    Update update.meta.js

--- update.meta.js
+++ update.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Update and Download URL Metadata Test
 // @version     1
-// @updateURL   https://gist.github.com/raw/3906612/update-meta-test.meta.js
-// @downloadURL https://gist.github.com/raw/3906612/update-meta-test.user.js
+// @updateURL   https://raw.githubusercontent.com/28dec/qldt_prepare_schedule/master/update.meta.js
+// @downloadURL https://github.com/28dec/qldt_prepare_schedule/raw/master/tampermonkey.user.js
 // ==/UserScript==
